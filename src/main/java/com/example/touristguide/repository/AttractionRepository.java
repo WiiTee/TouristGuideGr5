c4499fd0dd564f98203e5c7eae39061b0cd59128
MaVirgil <magnusvirgil@hotmail.com>
2025-08-27 13:11:12 +0200

    core API functionality

---
+++ src/main/java/com/example/touristguide/repository/AttractionRepository.java
@@ -0,0 +1,66 @@
+package com.example.touristguide.repository;
+
+import com.example.touristguide.model.TouristAttraction;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class AttractionRepository {
+
+    private final ArrayList<TouristAttraction> attractions = new ArrayList<>();
+
+
+    public AttractionRepository(){
+        populateRepository();
+    }
+
+    private void populateRepository() {
+        attractions.add(new TouristAttraction("Tivoli", "københavns største forlystelsespark"));
+        attractions.add(new TouristAttraction("Den lille Havfrue", "Danmarks mindste havfrue"));
+        attractions.add(new TouristAttraction("Råbjerg mile", "Danmarks største slette (der flytter sig)"));
+    }
+
+    public ArrayList<TouristAttraction> getAttractions() {
+        return attractions;
+    }
+
+    public TouristAttraction getAttractionByName(String name) {
+
+        for (TouristAttraction attraction : attractions) {
+            if (attraction.getName().equalsIgnoreCase(name)) return attraction;
+        }
+
+        return null;
+    }
+
+    public TouristAttraction addAttraction(TouristAttraction attraction) {
+        attractions.add(attraction);
+        return attraction;
+    }
+
+    public TouristAttraction editAttractionName(String name, String newName) {
+        TouristAttraction attractionToEdit = getAttractionByName(name);
+        if (attractionToEdit != null) {
+            attractionToEdit.setName(newName);
+        }
+        return attractionToEdit;
+    }
+
+    public TouristAttraction editAttractionDescription(String attractionName, String newDescription) {
+        TouristAttraction attractionToEdit = getAttractionByName(attractionName);
+        if (attractionToEdit != null) {
+            attractionToEdit.setDescription(newDescription);
+        }
+        return attractionToEdit;
+    }
+
+    public TouristAttraction deleteAttraction(String attractionName) {
+        TouristAttraction attractionToDelete = getAttractionByName(attractionName);
+        attractions.remove(attractionToDelete);
+        return attractionToDelete;
+    }
+
+}
+
+
