3430052f64c9886c89a268a7e7ce281dddec8513
MaVirgil <magnusvirgil@hotmail.com>
2025-08-29 10:47:29 +0200

    remove ternary in return statement of endpoints

--- src/main/java/com/example/touristguide/controller/AttractionController.java
+++ src/main/java/com/example/touristguide/controller/AttractionController.java
@@ -29,13 +29,17 @@ public class AttractionController {
     @GetMapping("/{name}")
     public ResponseEntity<TouristAttraction> getAttractionByName(@PathVariable String name) {
         TouristAttraction attraction = this.service.getAttractionByName(name);
-        return new ResponseEntity<>(attraction, (attraction == null) ? HttpStatus.BAD_REQUEST : HttpStatus.OK);
+
+        HttpStatus httpResponseCode = HttpStatus.BAD_REQUEST;
+        if (attraction != null) httpResponseCode = HttpStatus.OK;
+
+        return new ResponseEntity<>(attraction, httpResponseCode);
     }
 
     //POST
     @PostMapping("/add")
     public ResponseEntity<TouristAttraction> addAttraction (@RequestBody TouristAttraction attraction) {
-        if (attraction.getName() == null)  {
+        if (attraction.getName() == null || attraction.getName().isEmpty())  {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(this.service.addAttraction(attraction), HttpStatus.CREATED);
@@ -50,12 +54,20 @@ public class AttractionController {
     @PostMapping("/update/description/{name}")
     public ResponseEntity<TouristAttraction> editAttractionDescription(@PathVariable String name, @RequestBody TouristAttraction withNewDescription) {
         TouristAttraction updatedAttraction = this.service.editAttractionDescription(name, withNewDescription.getDescription());
-        return new ResponseEntity<>(updatedAttraction, (updatedAttraction == null) ? HttpStatus.BAD_REQUEST : HttpStatus.OK);
+
+        HttpStatus httpResponseCode = HttpStatus.BAD_REQUEST;
+        if (updatedAttraction != null) httpResponseCode = HttpStatus.OK;
+
+        return new ResponseEntity<>(updatedAttraction, httpResponseCode);
     }
 
     @PostMapping("/delete/{name}")
     public ResponseEntity<TouristAttraction> deleteAttraction(@PathVariable String name) {
         TouristAttraction attractionToDelete = this.service.deleteAttraction(name);
-        return new ResponseEntity<>(attractionToDelete, (attractionToDelete == null) ? HttpStatus.BAD_REQUEST : HttpStatus.OK);
+
+        HttpStatus httpResponseCode = HttpStatus.BAD_REQUEST;
+        if (attractionToDelete != null) httpResponseCode = HttpStatus.OK;
+
+        return new ResponseEntity<>(attractionToDelete, httpResponseCode);
     }
 }
